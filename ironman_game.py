10d76f197310f431a0272f6654ce5ca30c31be4e
vinayak <vinayakshukla1239@gmail.com>
2023-08-26 17:19:02 +0530

    26

--- ironman_game.py
+++ ironman_game.py
@@ -16,7 +16,7 @@ def draw():
     if game_state == -1:
         start_screen()
     elif game_state == 0:
-        screen.fill('black')  # Space-themed background
+        screen.fill('black')  
         p.draw()
         e.draw()
         c.draw()
@@ -24,7 +24,7 @@ def draw():
     elif game_state == 1:
         screen.draw.text('Game Over', (w // 2, h // 2), color='red', fontsize=60)
     elif game_state == 2:
-        screen.draw.text('You Win', (w // 2, h // 2), color='green', fontsize=60)
+        screen.draw.text('You Win', (w // 2, h // 2), color='yellow', fontsize=60)
 
 def update():
     global game_state
@@ -34,8 +34,8 @@ def update():
         score_update()
 
 def start_screen():
-    screen.fill('black')  # Space-themed start screen
-    screen.draw.text('Space Adventure Game', (w // 2, h // 2 - 60), color='white', fontsize=60, align='center')
+    screen.fill('black') 
+    screen.draw.text('Space Adventure Game', (w // 2, h // 2 - 60), color='blue', fontsize=60, align='center')
     screen.draw.text('Press SPACE to Start', (w // 2, h // 2 + 60), color='cyan', fontsize=40, align='center')
 
 def on_key_down(key):
@@ -45,13 +45,13 @@ def on_key_down(key):
 
 def enemy_update():
     if c.x > e.x:
-        e.x += 2
+        e.x += 3
     if c.x < e.x:
-        e.x -= 2
+        e.x -= 3
     if c.y > e.y:
-        e.y += 2
+        e.y += 3
     if c.y < e.y:
-        e.y -= 2
+        e.y -= 3
 
 def score_update():
     global score, game_state
@@ -59,29 +59,29 @@ def score_update():
         score += 10
         c.x = randint(0, w)
         c.y = randint(0, h)
-        sounds.action.play()  # Use a sound that exists
+        sounds.action.play()  
     if e.colliderect(c):
         score -= 10
         c.x = randint(0, w)
         c.y = randint(0, h)
-        sounds.action.play()  # Use a sound that exists
-    if score <= -50:
+        sounds.action.play()  
+    if score <= -60:
         game_state = 1
-    if score >= 70:
+    if score >= 100:
         game_state = 2
 
 def player_update():
     global p
     if keyboard.left:
-        p.x -= 7
+        p.x -= 8
     elif keyboard.right:
-        p.x += 7
+        p.x += 8
     elif keyboard.up:
-        p.y -= 7
+        p.y -= 8
     elif keyboard.down:
-        p.y += 7
+        p.y += 8
     
-    # Wrap around the screen
+    
     if p.right < 0:
         p.left = WIDTH
     if p.left > WIDTH:
